ca3e3682f8ae4926039a823eff92b1217fdaebd1
Joe Sak <joe@joesak.com>
2012-07-02 10:11:41 -0500

    Add the protection module

--- lib/protection.rb
+++ lib/protection.rb
@@ -1,5 +1,23 @@
 require "protection/version"
 
-module Protection
-  # Your code goes here...
+module Neoteric
+  module Protection
+    extend ActiveSupport::Concern
+
+    included do
+      helper_method :staging?
+      before_filter :require_http_basic_auth
+    end
+
+    private
+    def require_http_basic_auth
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV['STAGING_USERNAME'] && password == ENV['STAGING_PASSWORD']
+      end if staging?
+    end
+
+    def staging?
+      Rails.env.staging?
+    end
+  end
 end
