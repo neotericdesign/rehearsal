f49a16081a9bb0d474b9450a48c10b16dd50cbdb
Joe Sak <joe@joesak.com>
2012-12-20 10:08:14 -0600

    Add rails engine logic and view helpers

---
+++ lib/rehearsal.rb
@@ -0,0 +1,35 @@
+require "rehearsal/version"
+require "rehearsal/engine"
+
+module Rehearsal
+  def self.included(base)
+    base.extend ClassMethods
+    base.helper_method :staging?
+    base.before_filter :require_http_basic_auth
+  end
+
+  module ClassMethods
+    def rehearse_with(options = {})
+      class_eval do
+        define_method(:username) do
+          options[:username]
+        end
+
+        define_method(:password) do
+          options[:password]
+        end
+      end
+    end
+  end
+
+  private
+  def require_http_basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == self.username && password == self.password
+    end# if staging?
+  end
+
+  def staging?
+    Rails.env.staging?
+  end
+end
