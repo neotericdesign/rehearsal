f7bfac11a82ecd13f43cb6128dd42ca8976f7cfd
Joe Sak <joe@joesak.com>
2012-12-20 18:13:00 -0600

    Fix staging_banner bug wasn't restricted to staging env

--- lib/rehearsal/view_helpers.rb
+++ lib/rehearsal/view_helpers.rb
@@ -1,6 +1,8 @@
 module Rehearsal
   module ViewHelpers
     def staging_banner(options = {})
+      return unless staging?
+
       content_tag :div, :id => 'rehearsal-staging-banner' do
         options[:message] || default_message
       end
