91abf56ffaa1b4a18555d8b4a43b19e9c5bfaad8
Joe Sak <joe@joesak.com>
2013-05-08 10:05:39 -0500

    Allow config to read from hidden .rehearsal user config

--- lib/rehearsal/configuration.rb
+++ lib/rehearsal/configuration.rb
@@ -3,9 +3,31 @@ module Rehearsal
     attr_accessor :auth_envs, :banner_envs, :enabled
 
     def initialize
+      initialize_from_hidden_file!
+
       @auth_envs   ||= [:staging]
       @banner_envs ||= [:staging]
-      @enabled       = !@enabled.nil?
+      @enabled       = true if enabled.nil?
+    end
+
+    private
+    def initialize_from_hidden_file!
+      return unless File.exist?(hidden_file_path)
+
+      file    = File.open(hidden_file_path)
+      config  = YAML.load(file)
+
+      config.each do |attr, value|
+        if value.respond_to?(:split)
+          value = value.split(',').map(&:strip).map(&:to_sym)
+        end
+
+        self.send("#{attr}=", value)
+      end
+    end
+
+    def hidden_file_path
+      "#{Rails.root}/.rehearsal"
     end
   end
 end
